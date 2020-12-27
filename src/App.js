11b81468621f927fa02820f2a2c7a1c581e5a283
Surajit Karuri <surajitkaruri@gmail.com>
2020-12-28 03:20:17 +0530

    Pagination view of random post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Pagination from "./components/Pagination";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [postlist, setPostlist] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [postPerPage, setPostPerPage] = useState(5);
+  const [displayPage, setDisplayPage] = useState(1);
+  // const [pageToDisplay, setPageToDisplay] = useState([]);
+
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  let maxPostIndex = postPerPage * displayPage;
+  let minPostIndex = maxPostIndex - postPerPage;
+
+  // console.log(postlist);
+  // setPostDisplay(postlist.slice(minPostIndex, maxPostIndex));
+
+  useEffect(() => {
+    const fetchpost = async () => {
+      try {
+        setIsLoading(true);
+        // {data: d}=axios
+        const { data } = await axios.get(url);
+        setPostlist(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchpost();
+  }, []);
+
+  return isLoading ? (
+    <div className="container mt-auto">Loading...</div>
+  ) : (
+    <div className="container">
+      <div className="justify-content-evenly my-3">
+        <span>Select post per page:</span>
+        <div className="btn-group">
+          <input
+            type="button"
+            value="5"
+            className="btn btn-outline-primary"
+            onClick={() => setPostPerPage(5)}
+          />
+          <input
+            type="button"
+            value="10"
+            className="btn btn-outline-primary"
+            onClick={() => setPostPerPage(10)}
+          />
+          <input
+            type="button"
+            value="15"
+            className="btn btn-outline-primary"
+            onClick={() => setPostPerPage(15)}
+          />
+        </div>
+      </div>
+      <h3>Posts: </h3>
+      <div className="row row-cols-1 row-cols-md-3 g-4 border border-info my-2 pb-4">
+        {postlist.slice(minPostIndex, maxPostIndex).map((post) => (
+          <div className="col" key={post.id}>
+            <div className="card h-100">
+              <div className="card-body">
+                <h5 className="card-title">{post.title}</h5>
+                <p className="card-text">{post.body}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Pagination
+        totalposts={postlist.length}
+        postPerPage={postPerPage}
+        pageToDisplay={(page) => setDisplayPage(page)}
+      />
     </div>
   );
 }
---
+++ src/components/Pagination.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Pagination({ totalposts, postPerPage, pageToDisplay }) {
+  const totalPages = totalposts / postPerPage;
+  const pageList = [];
+
+  for (let i = 0; i < totalPages; i++) {
+    pageList.push(i + 1);
+  }
+
+  return (
+    <div className="pagination justify-content-end mt-4 mb-5">
+      {pageList.map((page) => (
+        <div className="page-item" key={page}>
+          <a href="#" className="page-link" onClick={() => pageToDisplay(page)}>
+            {page}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Pagination;
